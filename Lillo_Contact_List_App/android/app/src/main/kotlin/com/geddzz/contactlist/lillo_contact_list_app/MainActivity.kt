072693da8b1e419e790fdddd8197b5da7b34d429
geddzz <gerardvince.lillo@unc.edu.ph>
2022-04-30 16:45:10 +0800

    first commit

---
+++ Lillo_Contact_List_App/android/app/src/main/kotlin/com/geddzz/contactlist/lillo_contact_list_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.geddzz.contactlist.lillo_contact_list_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Lillo_Contact_List_App/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple Contact List App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Simple Contact List App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Let the Fun Begins!',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
