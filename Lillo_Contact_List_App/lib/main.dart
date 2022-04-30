bd4099cf6c5060688dcda9bcc473d875afeade2c
geddzz <gerardvince.lillo@unc.edu.ph>
2022-04-30 19:21:04 +0800

    second commit

--- Lillo_Contact_List_App/lib/main.dart
+++ Lillo_Contact_List_App/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -31,18 +33,72 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  TextEditingController name_controller = TextEditingController();
+  TextEditingController number_controller = TextEditingController();
+  TextEditingController email_controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("Simple Contact List App"),
       ),
       body: Container(
         padding: EdgeInsets.all(20),
         child: Column(
           children: <Widget>[
-            Text(
-              'Let the Fun Begins!',
+            TextField(
+              controller: name_controller,
+              decoration: InputDecoration(
+                labelText: "Contact Name",
+                hintText: "Enter Contact Name",
+                labelStyle: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black
+                )
+              ),
+              keyboardType: TextInputType.name,
+              obscureText: false, // Hides Input
+              //maxLength: 50, // Limits Character
+            ),
+            TextField(
+              controller: number_controller,
+              decoration: InputDecoration(
+                  labelText: "Mobile Number",
+                  hintText: "Enter Mobile Number",
+                  labelStyle: TextStyle(
+                    fontSize: 15,
+                    color: Colors.black
+                  )
+              ),
+              keyboardType: TextInputType.phone,
+              obscureText: false, // Hides Input
+            ),
+            TextField(
+              controller: email_controller,
+              decoration: InputDecoration(
+                  labelText: "Email Address",
+                  hintText: "Enter Email Address",
+                  labelStyle: TextStyle(
+                    fontSize: 15,
+                    color: Colors.black
+                  )
+              ),
+              keyboardType: TextInputType.emailAddress,
+              obscureText: false, // Hides Input
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            RaisedButton(onPressed: (){
+              print("Contact Name: " + name_controller.text);
+              print("Mobile Number: " + number_controller.text);
+              print("Email Address: " + email_controller.text);
+            },
+            child: const Text(
+                "Submit",
+              style: TextStyle(fontSize: 20),
+              ),
             ),
           ],
         ),
